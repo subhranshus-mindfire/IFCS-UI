3836b3238dd0a510be33c56e11d5453f921c753e
subhranshu <subhranshus@mindfiresolutions.com>
2025-11-13 12:06:47 +0530

    feat: enable msw mock api support

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
-import { StrictMode, Suspense } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import "../src/i18next.ts";
-import { pdfjs } from "react-pdf";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url
-).toString();
+async function enableMocking() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import('./mocks/browser')
+    await worker.start({
+      onUnhandledRequest: 'bypass',
+    })
+    console.log('%c[MSW] Mock API enabled', 'color: #66cdaa')
+  }
+}
 
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
+enableMocking().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root')!)
+  root.render(
+    <React.StrictMode>
       <App />
-    </Suspense>
-  </StrictMode>
-);
+    </React.StrictMode>
+  )
+})
